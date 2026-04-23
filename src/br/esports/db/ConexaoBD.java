a6662b46bf8228f433ce12ce222d6e7393cf8488
Ryan-nextLvl <ryan.aquitwt@gmail.com>
2026-04-23 16:16:00 -0300

    fix: remove senha da conexão JDBC (PostgreSQL sem senha)

--- src/br/esports/db/ConexaoBD.java
+++ src/br/esports/db/ConexaoBD.java
@@ -8,7 +8,7 @@ public class ConexaoBD {
 
     private static final String URL    = "jdbc:postgresql://localhost:5432/esports_db";
     private static final String USUARIO = "postgres";
-    private static final String SENHA   = "postgres";
+    private static final String SENHA   = "";
 
     public static Connection getConexao() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
