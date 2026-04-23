b0ab71f32382fc1b748a36d4a0a6b0d449fa4f7d
Ryan-nextLvl <ryan.aquitwt@gmail.com>
2026-04-23 16:36:50 -0300

    fix: força carregamento explícito do driver PostgreSQL via Class.forName

--- src/br/esports/db/ConexaoBD.java
+++ src/br/esports/db/ConexaoBD.java
@@ -6,9 +6,17 @@ import java.sql.SQLException;
 
 public class ConexaoBD {
 
-    private static final String URL    = "jdbc:postgresql://localhost:5432/esports_db";
+    private static final String URL     = "jdbc:postgresql://localhost:5432/esports_db";
     private static final String USUARIO = "postgres";
     private static final String SENHA   = "";
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Driver PostgreSQL não encontrado. Verifique se o .jar está no classpath.", e);
+        }
+    }
 
     public static Connection getConexao() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
